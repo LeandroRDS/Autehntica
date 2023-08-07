8692805951b84c539db937ffbf80ae74b6890022
leandrords <leandrords23012002@gmail.com>
2023-08-07 18:59:15 -0300

    botão minha conta div criada e botao voltar

--- javaScript/script-mobile.js
+++ javaScript/script-mobile.js
@@ -1,6 +1,13 @@
 /*Script Botão Minha Conta */
 var btn_minha_conta = window.document.querySelector('div#img-login')
 btn_minha_conta.addEventListener('click', fazerLogin)
+var home_mobile = window.document.querySelector('main#id-main-home-mobile')
+var menu_minha_conta = window.document.querySelector('div#id-div-minha-conta')
 function fazerLogin(){
-    location.href = "minha-conta.html" 
+     home_mobile.style.display = 'none'
+     menu_minha_conta.style.display = 'block'
+}
+function voltaTelaHome(){
+    menu_minha_conta.style.display = 'none'
+    home_mobile.style.display = ''
 }
