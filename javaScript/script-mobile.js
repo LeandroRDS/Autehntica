a5a2d0f9dedd2b2529353810fa4fc0670ca16ca8
leandrords <leandrords23012002@gmail.com>
2023-08-16 17:58:17 -0300

    começãndo a estilizar a tala façã login  logim

--- javaScript/script-mobile.js
+++ javaScript/script-mobile.js
@@ -1,9 +1,10 @@
+
 /*Script Botão Minha Conta */
 var btn_minha_conta = window.document.querySelector('div#img-login')
-btn_minha_conta.addEventListener('click', fazerLogin)
+btn_minha_conta.addEventListener('click', minhaConta)
 var home_mobile = window.document.querySelector('main#id-main-home-mobile')
 var menu_minha_conta = window.document.querySelector('div#id-div-minha-conta')
-function fazerLogin(){
+function minhaConta(){
      home_mobile.style.display = 'none'
      menu_minha_conta.style.display = 'block'
 }
@@ -11,3 +12,15 @@ function voltaTelaHome(){
     menu_minha_conta.style.display = 'none'
     home_mobile.style.display = ''
 }
+
+/* Script Botão Fazer Login*/
+btn_fazer_login = window.document.querySelector('div#id-div-login')
+btn_fazer_login.addEventListener('click', fazerLogin)
+function fazerLogin(){
+    location.href = 'tela-fazer-login.html'
+}
+
+function input_login(){
+    
+}
+
